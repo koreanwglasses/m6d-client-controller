553d73555fb42f4523dae3ceb10f4393a4c4bc1a
koreanwglasses <koreanwglasses@gmail.com>
2020-03-31 17:10:40 -0400

    Using https server

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,15 @@
-console.log("Hello World");
+const root = document.getElementById("react-root") as HTMLDivElement;
+
+root.innerText = "Loaded!";
+
+window.addEventListener(
+  "deviceorientation",
+  event => {
+    root.innerText =
+      `absolute: ${event.absolute}\n` +
+      `alpha: ${event.alpha}\n` +
+      `beta: ${event.beta}\n` +
+      `gamma: ${event.gamma}`;
+  },
+  true
+);
